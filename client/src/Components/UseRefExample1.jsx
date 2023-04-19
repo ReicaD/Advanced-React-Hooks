ff20a6652196a20b9ce37dd59ad17aef9986aabe
ReicaD <dbamanya447@gmail.com>
2023-04-18 23:26:41 -0400

    React app & bootstrap

---
+++ client/src/Components/UseRefExample1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UseRefExample1() {
+  return (
+    <div>
+        <form>
+            <label htmlFor="name">Name</label>
+            input.form-control
+        </form>
+    </div>
+  )
+}
+
+export default UseRefExample1
