ebe44eb1103ce76067a0936a8dca727ac0710634
ReicaD <dbamanya447@gmail.com>
2023-04-19 00:08:49 -0400

    Added UseRef for submittion

--- client/src/Components/UseRefExample1.jsx
+++ client/src/Components/UseRefExample1.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-
+import { useRef } from "react";
+import React from "react";
+//useRef returns an object of current
 function UseRefExample1() {
+  const inputRef = useRef();
+  const paraRef=useRef()
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    inputRef.current.value = "We are One";
+    inputRef.current.style.backgroundColour = "blue";
+    paraRef.current.innerText = "alli";
+  };
   return (
     <div>
-        <form>
-            <label htmlFor="name">Name</label>
-            input.form-control
-        </form>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          ref={inputRef}
+          id="name"
+          className="form-control mb-2"
+        />
+        <button type="submit" className="btn btn-success">
+          Submit
+        </button>
+        <p onClick={() => inputRef.current.focus()} ref={paraRef}>
+          {" "}
+          Hello
+        </p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default UseRefExample1
+export default UseRefExample1;
